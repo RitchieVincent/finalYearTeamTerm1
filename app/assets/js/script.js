cdcc1fa9dfa4b935e24340c20f6475a08a3366a3
Ritchie Vincent <rfcvincent@gmail.com>
2014-10-28 11:58:32 +0000

    Modified JS for party select

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,3 +1,11 @@
 function myFunction() {
-    // alert("hello");
+    if(document.getElementById('labourSelect').checked) {
+  		alert("Labour");
+	}else if(document.getElementById('conservativeSelect').checked) {
+		alert("Conservative");
+	} else if(document.getElementById('libdemSelect').checked) {
+		alert("Lib Dem");
+	} else if(document.getElementById('ukipSelect').checked) {
+		alert("UKIP");
+	} 
 }
