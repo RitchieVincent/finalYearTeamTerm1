a3233d2ee27e6d2b89286c0e98b492bfa8a67d82
Ritchie Vincent <rfcvincent@gmail.com>
2014-10-30 01:45:32 +0000

    Submit button starts animation

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,11 +1,14 @@
 function myFunction() {
-    if(document.getElementById('labourSelect').checked) {
-  		alert("Labour");
-	}else if(document.getElementById('conservativeSelect').checked) {
-		alert("Conservative");
-	} else if(document.getElementById('libdemSelect').checked) {
-		alert("Lib Dem");
-	} else if(document.getElementById('ukipSelect').checked) {
-		alert("UKIP");
-	} 
+ //    if(document.getElementById('labourSelect').checked) {
+ //  		alert("Labour");
+	// }else if(document.getElementById('conservativeSelect').checked) {
+	// 	alert("Conservative");
+	// } else if(document.getElementById('libdemSelect').checked) {
+	// 	alert("Lib Dem");
+	// } else if(document.getElementById('ukipSelect').checked) {
+	// 	alert("UKIP");
+	// } 
+
+	var d = document.getElementById("paperPartRight");
+	d.className = d.className + " paperPartRightAnimation";
 }
